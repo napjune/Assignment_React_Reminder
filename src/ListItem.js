816843a96c3bcb4178e67a4181d5a98b882f26b2
Nap <pornouma.p@ku.th>
2018-06-20 07:43:18 +0700

    change_secondaryaction

--- src/ListItem.js
+++ src/ListItem.js
@@ -53,23 +53,19 @@ export default class ListItem extends React.Component {
             
         </FormGroup>
         
-        <ListItemSecondaryAction className="EditButton">
-        <Label>
+        <ListItemSecondaryAction  className="EditButton">
+        
                 <IconButton mini aria-label="Edit" onClick={()=>this.props.editSetup(item)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
                     </svg>
                 </IconButton>
-            </Label>
-            </ListItemSecondaryAction>
-        
-        <ListItemSecondaryAction>
-        <Label>
+           
                 <IconButton color="secondary" aria-label="Delete" onClick={(e) => this.delete(e,item.task_id)} > 
                     <DeleteIcon />
                 </IconButton>
             
-            </Label>
+          
         </ListItemSecondaryAction>
        
       </Form>
